336f44443997872480f520400ef92622c9c28e64
Wolf <d.wolftripp@gmail.com>
2022-08-12 13:19:04 -0700

    add ternary solution to 7

--- js/app.js
+++ js/app.js
@@ -115,6 +115,10 @@ const sameData = data.reduce((allData, data) => {
   return allData
 }, {})
 
+// const sameData = data.reduce((allData, data) => {
+//   allData[data] = allData[data] ? allData[data] + 1 : 1
+// }, {})
+
 console.log("7.", sameData)
 
 const devs = [
