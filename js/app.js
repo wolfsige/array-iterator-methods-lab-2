5350a0b3819d063e79ee4550037c4c94ad502336
Wolf <d.wolftripp@gmail.com>
2022-08-22 13:15:21 -0700

    complete 10

--- js/app.js
+++ js/app.js
@@ -157,7 +157,9 @@ const comments = [
 // Array.prototype.find()
 // 10. Find the comment with the id of 823423.
 
+const findId = comments.find(comments => comments.id === 823423)
 
+console.log("10.", findId)
 
 // Array.prototype.findIndex()
 // 11. Find the index of the comment with an id of 123523.
